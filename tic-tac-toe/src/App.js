519cac1aee51a430958cacb057987d4f130a92d4
nxm403 <nxm403@case.edu>
2022-12-05 18:23:43 -0500

    showing scoreboard

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import './App.css';
 
 import { Board } from './components/Board';
+import { ScoreBoard } from './components/ScoreBoard';
 
 function App() {
   const WIN_CONDITIONS = [
@@ -18,6 +19,7 @@ function App() {
 
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xPlaying, setXPlaying] = useState(true);
+  const [scores, setScores] = useState({xScore: 0, oScore: 0})
 
   const handleBoxClick = (boxIdx) => {
     const updatedBoard = board.map((value, idx) => {
@@ -27,6 +29,22 @@ function App() {
         return value;
       }
     })
+
+    const winner = checkWinner(updatedBoard);
+
+    if(winner) {
+      if(winner === "O") {
+        let {oScore} = scores;
+        oScore += 1;
+        setScores({...scores, oScore})
+      } else {
+        let {xScore} = scores;
+        xScore += 1;
+        setScores({...scores, xScore})
+      }
+    }
+
+    console.log(scores);
 
     checkWinner(updatedBoard);
     setBoard(updatedBoard);
@@ -47,6 +65,7 @@ function App() {
 
   return (
     <div className="App">
+      <ScoreBoard scores={scores} xPlaying={xPlaying}/>
       <Board board={board} onClick={handleBoxClick}></Board>
       
     </div>
