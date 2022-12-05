e86ffc029d19b2369d9991f67585dc973996ca2c
nxm403 <nxm403@case.edu>
2022-12-05 17:36:19 -0500

    base structure of game w/ no functionality

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import { Board } from './components/Board';
+
+function App() {
+  const board = ["X","X","X","X","X","X","X","X","X"]
+
+  return (
+    <div className="App">
+      <Board board={board} onClick={null}></Board>
+      
+    </div>
+  );
+}
+
+export default App;
