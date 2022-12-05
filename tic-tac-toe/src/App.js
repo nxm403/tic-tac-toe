4c166c3cf7509dfcdf04b5a84cf245713a018975
nxm403 <nxm403@case.edu>
2022-12-05 18:04:59 -0500

    Able to detect winner now

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -5,6 +5,17 @@ import './App.css';
 import { Board } from './components/Board';
 
 function App() {
+  const WIN_CONDITIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xPlaying, setXPlaying] = useState(true);
 
@@ -17,9 +28,22 @@ function App() {
       }
     })
 
+    checkWinner(updatedBoard);
     setBoard(updatedBoard);
     setXPlaying(!xPlaying);
+    
   };
+
+  const checkWinner = (board) => {
+    for(let i = 0; i < WIN_CONDITIONS.length; i++) {
+      const [x,y,z] = WIN_CONDITIONS[i];
+
+      if (board[x] && board[x] === board[y] && board[y] === board[z]) {
+        console.log(board[x]);
+        return board[x];
+      }
+    }
+  }
 
   return (
     <div className="App">
