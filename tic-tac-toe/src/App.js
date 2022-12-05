755acee4d2696d59ca7fe5cb43af6f214e50c501
nxm403 <nxm403@case.edu>
2022-12-05 17:59:49 -0500

    Adding game board logic

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,13 +1,29 @@
+import React, {useState} from 'react';
+
 import './App.css';
 
 import { Board } from './components/Board';
 
 function App() {
-  const board = ["X","X","X","X","X","X","X","X","X"]
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xPlaying, setXPlaying] = useState(true);
+
+  const handleBoxClick = (boxIdx) => {
+    const updatedBoard = board.map((value, idx) => {
+      if(idx === boxIdx) {
+        return xPlaying === true ? "X" : "O";
+      } else {
+        return value;
+      }
+    })
+
+    setBoard(updatedBoard);
+    setXPlaying(!xPlaying);
+  };
 
   return (
     <div className="App">
-      <Board board={board} onClick={null}></Board>
+      <Board board={board} onClick={handleBoxClick}></Board>
       
     </div>
   );
--- tic-tac-toe/src/components/Board.js
+++ tic-tac-toe/src/components/Board.js
@@ -7,7 +7,7 @@ export const Board = ({board, onClick}) => {
   return (
     <div className='board'>
         {board.map((value, idx) => {
-            return <Box value="X" onClick={() => onClick(idx)}/>
+            return <Box value={value} onClick={() => onClick(idx)}/>
         })}
         </div>
   )
